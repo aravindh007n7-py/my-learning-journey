65a8cf64a5ff726c3fe73461507dd3f50e4a4da9
aravindh007n7-py <aravindh007.n7@gmail.com>
2026-07-23 20:00:24 +0530

    day_1 commit

---
+++ python/Day_1/hello_world.py
@@ -0,0 +1,11 @@
+print("hello world")
+print("welcome to my python learning journey")
+print("expect cringe statements here")
+
+print('''
+'a guy who wants to learn python for ai/ml.
+soon i will be learning about linear algebra,probability,
+statistics for ml. 
+i will update my learning path along the way
+thank you 
+''')
